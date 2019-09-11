6e9f00db7228a953832858c793d4f6abaffb6c9e
Georgi <georgi.shahnazaryan@mail.ru>
2019-09-11 21:22:35 +0400

    Solved merge conflict in Hospital and Department models

--- src/main/java/am/initsolutions/models/Department.java
+++ src/main/java/am/initsolutions/models/Department.java
@@ -2,13 +2,22 @@ package am.initsolutions.models;
 
 import javax.persistence.*;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "department")
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String name;
 
     @ManyToOne
--- src/main/java/am/initsolutions/models/Hospital.java
+++ src/main/java/am/initsolutions/models/Hospital.java
@@ -1,18 +1,31 @@
 package am.initsolutions.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "hospital")
 public class Hospital {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String name;
+    @Column
     private String address;
+    @Column
     private String phoneNumber;
+    @Column
     private String openAt;
+    @Column
     private String closeAt;
 
     @OneToMany(mappedBy = "hospital")
