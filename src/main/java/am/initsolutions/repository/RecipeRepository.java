b960465e1b0012303815b54819da307525036b13
rozat <rozatahmazyan!@gmail.com>
2019-09-30 11:28:55 +0400

    add multi select for choosing medicine

--- src/main/java/am/initsolutions/repository/RecipeRepository.java
+++ src/main/java/am/initsolutions/repository/RecipeRepository.java
@@ -7,14 +7,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import javax.transaction.Transactional;
-
+import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe,Long> {
-     @Transactional
+    @Transactional
     @Modifying
     @Query(value ="INSERT INTO recipe(doctorId, description) VALUES(:doctorId,:description)",
             nativeQuery = true)
     Long saveRecipe(@Param("doctorId") Long doctorId);
 
-
+//    @Transactional
+//    @Modifying
+//    @Query(value ="INSERT INTO recipe_medicine(recipe_id,medicine_id) VALUES(:recipeId,:medicineId)",
+//            nativeQuery = true)
+//    void recipeMedicine(@Param("recipeId") Long recipeId,@Param("medicineId")Long medicineId);
 }
